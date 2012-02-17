b0a06a4ab3786e1f3bb7d9fdf184f0d178d36e5b
Marten Veldthuis <marten@veldthuis.com>
2012-02-17 15:45:59 +0100

    Write some env vars (static list for now)

--- lib/simplestats/middleware.rb
+++ lib/simplestats/middleware.rb
@@ -7,7 +7,12 @@ module SimpleStats
     end
 
     def call(env)
-      stat = SimpleStats::Stat.create(:rack_env => env)
+      stat = SimpleStats::Stat.create(server_address: env["SERVER_ADDR"],
+                                      server_port: env["SERVER_PORT"],
+                                      request: {
+                                        uri: env["REQUEST_URI"]
+                                      })
+
       status, headers, body = @app.call(env.merge("simplestats.stat_id" => stat.id.to_s))
       [status, headers, body]
     end
